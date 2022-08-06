321894e33402598c82aa1e2113c5c782a0082be1
Muhammet Ali Öztürk <mali_1234@outlook.com.tr>
2022-08-06 18:18:45 +0300

    ilk

---
+++ giris/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace giris
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.link = new System.Windows.Forms.TextBox();
+            this.mp3 = new System.Windows.Forms.RadioButton();
+            this.mp4 = new System.Windows.Forms.RadioButton();
+            this.indir = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(55, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(145, 56);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Link:";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Calibri", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(55, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 56);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Uzantı:";
+            // 
+            // link
+            // 
+            this.link.Location = new System.Drawing.Point(132, 60);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(517, 22);
+            this.link.TabIndex = 2;
+            // 
+            // mp3
+            // 
+            this.mp3.AutoSize = true;
+            this.mp3.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mp3.Location = new System.Drawing.Point(176, 160);
+            this.mp3.Name = "mp3";
+            this.mp3.Size = new System.Drawing.Size(64, 25);
+            this.mp3.TabIndex = 3;
+            this.mp3.TabStop = true;
+            this.mp3.Text = "Mp3";
+            this.mp3.UseVisualStyleBackColor = true;
+            this.mp3.CheckedChanged += new System.EventHandler(this.mp3_CheckedChanged);
+            // 
+            // mp4
+            // 
+            this.mp4.AutoSize = true;
+            this.mp4.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.mp4.Location = new System.Drawing.Point(276, 160);
+            this.mp4.Name = "mp4";
+            this.mp4.Size = new System.Drawing.Size(64, 25);
+            this.mp4.TabIndex = 4;
+            this.mp4.TabStop = true;
+            this.mp4.Text = "Mp4";
+            this.mp4.UseVisualStyleBackColor = true;
+            this.mp4.CheckedChanged += new System.EventHandler(this.mp4_CheckedChanged);
+            // 
+            // indir
+            // 
+            this.indir.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.indir.Location = new System.Drawing.Point(276, 261);
+            this.indir.Name = "indir";
+            this.indir.Size = new System.Drawing.Size(156, 53);
+            this.indir.TabIndex = 5;
+            this.indir.Text = "İndir";
+            this.indir.UseVisualStyleBackColor = true;
+            this.indir.Click += new System.EventHandler(this.indir_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(61, 333);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(588, 53);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(713, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.indir);
+            this.Controls.Add(this.mp4);
+            this.Controls.Add(this.mp3);
+            this.Controls.Add(this.link);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "video";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox link;
+        private System.Windows.Forms.RadioButton mp3;
+        private System.Windows.Forms.RadioButton mp4;
+        private System.Windows.Forms.Button indir;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ giris/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using VideoLibrary;
+using MediaToolkit;
+using System.IO;
+using MediaToolkit.Model;
+
+
+namespace giris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Boolean format = true;
+        private async void indir_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog() { Description = "Klasör seçiniz" })
+            {
+                if (dialog.ShowDialog() == DialogResult.OK)
+                {
+                    MessageBox.Show("İndirme Başladı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    var ytb = YouTube.Default;
+                    var video = await ytb.GetVideoAsync(link.Text);
+                    File.WriteAllBytes(dialog.SelectedPath + @"\" + video.FullName, await video.GetBytesAsync());
+
+                    var input = new MediaToolkit.Model.MediaFile { Filename = dialog.SelectedPath + @"\" + video.FullName };
+                    var output = new MediaToolkit.Model.MediaFile { Filename = $"{dialog.SelectedPath + @"\" + video.FullName}.mp3" };
+
+                    using (var engine = new Engine())
+                    {
+                        engine.GetMetadata(input);
+                        engine.Convert(input, output);
+                    }
+                    if (format == true)
+                    {
+                        File.Delete(dialog.SelectedPath + @"\" + video.FullName);
+                    }
+                    else
+                    {
+                        File.Delete($"{dialog.SelectedPath + @"\" + video.FullName}.mp3");
+
+                    }
+                    progressBar1.Value = 100;
+                    MessageBox.Show("İndirme Tamamlandı", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Doya Seçin", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private void mp3_CheckedChanged(object sender, EventArgs e)
+        {
+            format = true;
+        }
+
+        private void mp4_CheckedChanged(object sender, EventArgs e)
+        {
+            format = false;
+        }
+    }
+}
